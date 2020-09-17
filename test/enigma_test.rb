985b653d69b01b024e7e20eb35bee8330fcff19d
todd-estes <elestes@gmail.com>
2020-09-16 21:01:30 -0600

    Add enigma_test.rb

---
+++ test/enigma_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/enigma'
+
+class EnigmaTest < Minitest::Test
+  def test_it_exists
+
+    assert_instance_of Enigma, self
+  end
+end
+a
