b1a9b220b4877ce90312b185236074566fe634bb
todd-estes <elestes@gmail.com>
2020-09-18 18:18:54 -0600

    Update enigma_test.rb and enigma.rb with convert into keys method and test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,12 +4,24 @@ class Enigma
   end
 
   def get_date
-    Date.today.strftime("%m%d%y")
+    Date.today.strftime("%d%m%y")
   end
 
-  def generate_key
+  def create_key
     rand(99999).to_s.rjust(5, "0")
   end
+
+  def make_offset
+    num = ((get_date.to_i)**2)
+    (num.to_s).slice(-4, 4)
+  end
+
+  def convert_into_keys
+    create_key.chars.each_cons(2).map do |pair|
+      pair.join.to_i
+    end
+  end
+
 
 
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -4,26 +4,37 @@ require './lib/enigma'
 
 class EnigmaTest < Minitest::Test
   def test_it_exists
-
      enigma = Enigma.new
      assert_instance_of Enigma, enigma
   end
 
 
   def test_it_finds_the_date
-    enigma = Enigma.new
+    skip
 
-    assert_equal "091720", enigma.get_date
+    # Date.stubs(:today).returns(Date.new(2020,2,24))
+    enigma = Enigma.new
+    skip
+    assert_equal "180920", enigma.get_date
   end
 
   def test_it_generates_a_five_digit_number
     enigma = Enigma.new
 
     srand(99999)
-    assert_equal "19787", enigma.generate_key
+    assert_equal "19787", enigma.create_key
   end
 
+  def test_it_makes_an_offset
+    skip
+    enigma = Enigma.new
 
+    assert_equal "8400", enigma.make_offset
+  end
 
-
+  def test_it_converts_to_keys
+    enigma = Enigma.new
+    create_key = 52397
+    assert_equal [38, 88, 80, 0], enigma.convert_into_keys
+  end
 end
