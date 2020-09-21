718393f2242f48914bb27e8f8935762fdc5abce5
todd-estes <elestes@gmail.com>
2020-09-20 21:06:37 -0600

    Add convert_keys method to shift_maker.rb

--- lib/shift_maker.rb
+++ lib/shift_maker.rb
@@ -4,4 +4,10 @@ class ShiftMaker
     num = ((date.to_i)**2)
     (num.to_s).slice(-4, 4)
   end
+
+  def convert_keys(key = create_key)
+    key.chars.each_cons(2).map do |pair|
+      pair.join.to_i
+    end
+  end
 end
