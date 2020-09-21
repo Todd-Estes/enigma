4453f03dfb3b82e0d716d22189aacf5b688525a1
todd-estes <elestes@gmail.com>
2020-09-20 21:25:26 -0600

    Add alphabet method test to shift_maker_test.rb

--- test/shift_maker_test.rb
+++ test/shift_maker_test.rb
@@ -9,6 +9,11 @@ class ShiftMakerTest < Minitest::Test
     @shift_maker = ShiftMaker.new
   end
 
+  def test_it_has_an_alphabet
+    expected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+    assert_equal expected, @enigma.alphabet
+  end
+
   def test_it_exists
     assert_instance_of ShiftMaker, @shift_maker
   end
@@ -29,7 +34,7 @@ class ShiftMakerTest < Minitest::Test
   end
 
   def test_it_can_shift_characters
-    assert_equal "a", @enigma.shift_characters("r", 10)
+    assert_equal "a", @shift_maker.shift_characters("r", 10)
   end
 
 
