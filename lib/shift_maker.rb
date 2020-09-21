e70db2c34f6d4e3b6d178476df94ef0ad70f5017
todd-estes <elestes@gmail.com>
2020-09-20 21:23:04 -0600

    Add shift_characters method to shift_maker.rb

--- lib/shift_maker.rb
+++ lib/shift_maker.rb
@@ -10,4 +10,18 @@ class ShiftMaker
       pair.join.to_i
     end
   end
+
+  def make_shifts(keys = self.create_key, date = self.get_date)
+    make_offset(date).chars.map.with_index do |char,ind|
+      char.to_i + (convert_keys(keys))[ind]
+    end
+  end
+
+  # def alphabet
+  #  ("a".."z").to_a << " "
+  # end
+
+  def shift_characters(character, shift)
+    self.alphabet[(self.alphabet.index(character) + shift) % 27]
+  end
 end
