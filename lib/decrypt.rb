129ac7d70f75f657d9d6f6c12964f6afc7d26bff
todd-estes <elestes@gmail.com>
2020-09-21 18:17:00 -0600

    Add decrypt.rb cli runner

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -2,29 +2,28 @@ require_relative './enigma'
 require_relative './shift_maker'
 
 
-$ ruby ./lib/decrypt.rb encrypted.txt decrypted.txt 82648 240818
-Created 'decrypted.txt' with the key 82648 and date 240818
-
 
 enigma = Enigma.new
 
 handle = File.open(ARGV[0], "r")
 
-message_key = ARGV[2]
+encryption_key = ARGV[2]
 
-message_ = ARGV[3]
+encryption_date = ARGV[3]
 
-incoming_message = handle.read.chomp
+encryption_message = handle.read.chomp
 
-encrypted_message = enigma.encrypt(incoming_message)
-# require "pry"; binding.pry
+
+
+
+decrypted_message = enigma.decrypt(encryption_message, encryption_key, encryption_date)
 
 handle.close
 
 writer = File.open(ARGV[1], "w")
 
-writer.write(encrypted_message)
+writer.write(decrypted_message[:decryption])
 
 writer.close
 
-puts "Created 'encrypted.txt' with the key #{encrypted_message[:key]} and date #{encrypted_message[:date]}"
+puts "Created 'decrypted.txt' with the key #{encryption_key} and date #{encryption_date}"
